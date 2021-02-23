918d0925b6d1a03d42524ab988ef68ded8e54c4d
vinayakmaskar <79522753+vinayakmaskar@users.noreply.github.com>
2021-02-23 22:53:02 +0530

    Implementation of stack using linked list.

---
+++ stackusinglinkedlist.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <malloc.h>
+
+
+struct node 
+{
+    int data;
+    struct node *next;
+};
+
+struct node *top = 0,*newnode,*temp;
+
+
+void push();
+void display();
+void peek();
+void pop();
+void main()
+{
+   
+     int choice;
+     printf("*****performing stack using linked list*****\n");
+     choice = 0;
+     
+
+     while (choice != 5)
+     {
+         printf("\nenter your choice\n");
+     printf("1.push\n2.pop\n3.display\n4.peek\n");
+     scanf("%d",&choice);
+
+     switch(choice)
+     {
+         case 1: 
+         {
+             push();
+             break;
+         }
+         case 2:
+         {
+             pop();
+             break;
+         }
+         case 3:
+         {
+             display();
+             break;
+         }
+         case 4:
+         {
+             peek();
+             break;
+         }
+         case 5:
+         {
+             printf("exiting......\n");
+         }
+         default :
+         {
+             printf("invalid choice\n");
+         }
+     };
+
+     }
+    
+}
+
+void push()
+{
+    
+   
+    
+    
+   
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("enter the data\n");
+        scanf("%d",&newnode -> data);
+        newnode -> next = top;
+        top = newnode;
+
+      
+
+
+    
+
+}
+
+void display()
+{
+    printf("printing the elements in the stack\n");
+    temp = top;
+
+    if (top == 0)
+    {
+        printf("stack is empty\n");
+    }
+    while (temp != 0)
+    {
+        printf("%d\n",temp -> data);
+        temp = temp -> next;
+    }
+    
+
+}
+
+void peek()
+{
+    temp = top;
+
+    if (top == 0)
+    {
+        printf("stack is empty\n");
+    }
+    else
+    printf("peek element is %d\n",temp -> data);
+
+}
+
+void pop()
+{
+    printf("performing pop operation\n");
+    temp = top;
+
+    top = top -> next;
+    printf("popped element is %d\n",temp -> data);
+    free(temp);
+    
+
+}
